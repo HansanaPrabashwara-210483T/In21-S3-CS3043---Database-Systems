50726a0e223cf478c3d4d1dc0b4815a77698605d
Kevin Fernando <94424408+JudeKFdo@users.noreply.github.com>
2023-10-27 08:08:33 +0530

    Add database connection into separate config/ folder
    Add database connection to separate config/ folder and remove connection-establishing code from index.js file. db.env should also be moved into config/ folder.

--- Airline Reservation System/backend/index.js
+++ Airline Reservation System/backend/index.js
@@ -1,19 +1,10 @@
 import express from "express"
-import mysql from "mysql2"
 import cors from "cors"
-import dotenv from "dotenv"
+import db from "./config/db.js";
 import authRouter from "./routes/auth.js";
 
-dotenv.config({ path: './db.env'});
 
 const app = express()
-
-const db = mysql.createConnection({
-    host        : process.env.DB_HOST,
-    user        : process.env.DB_USER,
-    password    : process.env.DB_PASSWORD,
-    database    : process.env.DB_NAME,
-});
 
 app.use(express.json());
 app.use(cors());
