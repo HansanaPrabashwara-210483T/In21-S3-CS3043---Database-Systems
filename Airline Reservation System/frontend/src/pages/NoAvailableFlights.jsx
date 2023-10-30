d9dae26f72444142a1cfd9dc94184525f014a81f
VilashNaveen <vilashnaveen@gmail.com>
2023-10-30 09:24:07 +0530

    Update NoFlightsAvailable page

--- Airline Reservation System/frontend/src/pages/NoAvailableFlights.jsx
+++ Airline Reservation System/frontend/src/pages/NoAvailableFlights.jsx
@@ -3,7 +3,11 @@
 import React from "react";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import "./Styles/NoFlightsAvailable.css";
+//import "./Styles/NoFlightsAvailable.css";
+import NavBar from "./Navbar";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+
 
 const NoFlightsPage = () => {
   const navigate = useNavigate();
@@ -12,15 +16,29 @@ const NoFlightsPage = () => {
     navigate(-1);
   };
 
-  return (
-    <div className="no-flights-page">
-      <h5>No Flights Available <br /> </h5>
-      <p>There are no flights available during the given time period. <br /> </p>
-      <Button variant="contained" onClick={handleReturn}>
-        Return
-      </Button>
-    </div>
-  );
+    return (
+        <>
+            <NavBar />
+            <Container component="main" maxWidth="xs">
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        marginBottom: "20vh",
+                    }}
+                ></Box>
+                <div className="no-flights-page">
+                    <h2>Sorry.... No Flights Available</h2>
+                    <p>There are no flights available during the given time period.</p>
+                    <br/>
+                    < Button type = "submit" onClick={handleReturn} fullWidth variant = "contained" sx = {{ mt: 6, mb: 2, backgroundColor:"black" }}> Return
+                    </Button>
+                </div>
+            </Container>
+        </>
+    );
 };
 
 export default NoFlightsPage;
