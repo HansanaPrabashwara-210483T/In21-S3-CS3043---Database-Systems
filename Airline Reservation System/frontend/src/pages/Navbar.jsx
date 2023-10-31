d029ed6122627b36f1e06bf71ca0e744606e676d
VilashNaveen <vilashnaveen@gmail.com>
2023-10-31 17:41:01 +0530

    Update ConditionalRender

--- Airline Reservation System/frontend/src/pages/Navbar.jsx
+++ Airline Reservation System/frontend/src/pages/Navbar.jsx
@@ -1,20 +1,46 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-//import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import HiveSharpIcon from '@mui/icons-material/HiveSharp';
 import logo from "../assets/logoNameSmall.png";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 // import { SiConsul } from "react-icons/si";
 // import { BsPhoneVibrate } from "react-icons/bs";
 // import { AiOutlineGlobal } from "react-icons/ai";
 // import { CgMenuGridO } from "react-icons/cg";
 
-const pages = ['Booking','Schedule', 'Help'];
-const urls = ['/booking', '/shedule','/help']
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
-
 const ResponsiveAppBar = () => {
 
     const navigate = useNavigate();
+
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
     const handleLogout = () => {
         localStorage.removeItem("token");
@@ -35,6 +61,12 @@ const ResponsiveAppBar = () => {
     }
     const handleHelpClick = (event) => {
         navigate('/help');
+    }
+    const handleSignInClick = (event) => {
+        navigate('/sign-in');
+    }
+    const handleSignUPClick = (event) => {
+        navigate('/sign-up');
     }
 
     const [noBg, addBg] = useState("navBar");
@@ -67,7 +99,47 @@ const ResponsiveAppBar = () => {
                     <li className="listItem" onClick={handleHelpClick}>
                         Help
                     </li>
-                    <button className="bttn flex btnTwo">Contact</button>
+
+                    {!localStorage.getItem("token")  && (
+                        <>
+                            <button className="bttn flex btnTwo" onClick={handleSignUPClick}>Sign Up</button>
+
+                            <button className="bttn flex btnTwo" onClick={handleSignInClick}>Sign In</button>
+                        </>
+                    )}
+                    {localStorage.getItem("token")  && (
+                        <>
+                            <Box sx={{ flexGrow: 0 }}>
+                                <Tooltip title="Open settings">
+                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                        <Avatar alt="A" />
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    sx={{ mt: '45px' }}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    <MenuItem onClick={handleLogout}>
+                                        <Typography textAlign="center">Logout</Typography>
+                                    </MenuItem>
+                                </Menu>
+                            </Box>
+                            <p style={{ whiteSpace: 'pre' }}>{"   " + localStorage.getItem("username")}</p>
+                        </>
+                    )}
+
                 </ul>
             </div>
         </div>
