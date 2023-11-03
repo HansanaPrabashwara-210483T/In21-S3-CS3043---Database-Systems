f1af47662313e56ccef98c8c032f592abc70fec8
HansanaPrabashwara <140901227+HansanaPrabashwara@users.noreply.github.com>
2023-11-03 09:56:53 +0530

    Updated the ticket

--- Airline Reservation System/frontend/src/pages/Ticket.jsx
+++ Airline Reservation System/frontend/src/pages/Ticket.jsx
@@ -1,144 +1,171 @@
-import * as React from "react";
-import NavBar from "./Navbar";
-import { Container, Box } from "@mui/material";
-import Grid from "@mui/material/Grid";
-import { useNavigate, useLocation } from "react-router-dom";
-import axios from "axios";
-import { useState } from "react";
-import { useEffect } from "react";
-import Typography from "@mui/material/Typography";
-import HiveSharpIcon from "@mui/icons-material/HiveSharp";
-import Toolbar from "@mui/material/Toolbar";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import Button from "@mui/material/Button";
+import * as React from 'react';
+import NavBar from './Navbar'
+import { Container,Box } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios'
+import {useState} from 'react'
+import {useEffect} from 'react'
+import Typography from '@mui/material/Typography';
+import HiveSharpIcon from '@mui/icons-material/HiveSharp';
+import Toolbar from '@mui/material/Toolbar';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Button from '@mui/material/Button';
+import logo from "../assets/ticketLogo.png";
 import Background from "../components/Background";
-import logo from "../assets/logoNameSmall.png";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function Ticket() {
-  const [booking, setBookings] = useState([]);
-  const [isButtonHidden, setIsButtonHidden] = useState(false);
+    const [booking,setBookings] = useState([])
+    const [isButtonHidden, setIsButtonHidden] = useState(false);
 
-  const location = useLocation();
-  const booking_id = location.pathname.split("/")[2];
 
-  useEffect(() => {
-    const fetchALLModels = async () => {
-      try {
-        const res = await axios.get(
-          "http://localhost:8000/customer_booking/" + booking_id
-        );
-        setBookings(res.data[0]);
-        console.log(res);
-      } catch (err) {
-        console.log(err);
-      }
+    const location = useLocation()
+    const booking_id = location.pathname.split("/")[2]
+
+    useEffect(() => {
+        const fetchALLModels = async() => {
+            try {
+                const res = await axios.get("http://localhost:8000/customer_booking/"+booking_id)
+                setBookings(res.data[0]);
+                console.log(res)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        fetchALLModels()
+    }, [])
+
+    console.log(booking["origin"]);
+    
+    const removeTAndZ = (dateString) => {
+        if(dateString === undefined) return("");
+        const [date, time] = dateString.split(/T|Z/);
+        const [t,sec] = time.split(".")
+        const [h,m,s] = t.split(":")
+        return date + " | " + h + " : " + m;
     };
-    fetchALLModels();
-  }, []);
 
-  console.log(booking["origin"]);
+    const printPage = () => {
+        window.print();
+    };
 
-  const removeTAndZ = (dateString) => {
-    if (dateString === undefined) return "";
-    const [date, time] = dateString.split(/T|Z/);
-    const [t, sec] = time.split(".");
-    const [h, m, s] = t.split(":");
-    return date + " | " + h + " : " + m;
-  };
+    const combineEvent = () => {
+        setIsButtonHidden(true);
+        setTimeout(() => {
+            printPage();
+        }, 50); 
+        setTimeout(() => {
+            setIsButtonHidden(false);
+        }, 1000); 
+    };
 
-  const printPage = () => {
-    window.print();
-  };
 
-  const combineEvent = () => {
-    setIsButtonHidden(true);
-    setTimeout(() => {
-      printPage();
-    }, 50);
-    setTimeout(() => {
-      setIsButtonHidden(false);
-    }, 1000);
-  };
+    return(
+        <>{!isButtonHidden && (
+            <><NavBar /><Background /></>
 
-  return (
-    <>
-      {!isButtonHidden && <NavBar />}
-      <Background />
-      <br></br>
-      <div className="ContainerTicket">
-        <div className="glassBox">
-          <Container>
-            <h1>Your Ticket </h1>
+        )}
+        <br></br>
+        
 
-            <Container
-              sx={{
-                justifyContent: "center",
-                display: "flex",
-              }}>
-              <Box className="models3">
-                <Container sx={{ color: "#fff" }}>
-                  <div className="logoDiv" style={{ width: "35%" }}>
-                    <img src={logo} alt="B Airways logo" className="logo" />
-                  </div>
-
-                  <Container>
-                    <Typography
-                      varient="h1"
-                      className="bigText"
-                      fontSize={30}
-                      fontFamily={"Ubuntu"}
-                      marginBottom={2}>
-                      {booking.origin}{" "}
-                      <ArrowForwardIcon sx={{ fontSize: "0.7em" }} />{" "}
-                      {booking.destination}
-                    </Typography>
-                    <Typography className="smallText" variant="p">
-                      Aircraft: {booking.call_sign}
-                      <br />
-                      Seat No: {booking.seat_class}-{booking.seat_number}
-                      <br />
-                      Class :{" "}
-                      {(booking.seat_class === "E" && "Economy") ||
-                        (booking.seat_class === "B" && "Business") ||
-                        (booking.seat_class === "P" && "Platinum")}
-                      <br />
-                      Departure : {removeTAndZ(booking.departure_time)}
-                      <br />
-                      Arrival : {removeTAndZ(booking.arrival_time)}
-                      <br />
-                    </Typography>
-                    <Typography className="smallText" variant="p">
-                      Name : {booking.name}
-                      <br />
-                      NIC : {booking.nic}
-                      <br />
-                      Passport Id : {booking.passport_id}
-                    </Typography>
-                  </Container>
-                </Container>
-              </Box>
-            </Container>
-          </Container>
-          <Container
-            sx={{
-              marginTop: "5vh",
-              justifyContent: "center",
-              display: "flex",
-            }}>
+            
+            <div className={!isButtonHidden &&("signinContainer")}>
+            <CssBaseline />
+            <div className={!isButtonHidden &&("glassBox")}>
+        <Container>
+        <Container sx={{ justifyContent:"center", display:"flex"}} className="signin">
             <Grid>
-              {!isButtonHidden && (
-                <button
-                  id="myButton"
-                  variant="contained"
-                  onClick={combineEvent}
-                  className="bttn bttnNew2">
-                  Print Ticket
-                </button>
-              )}
+      
+                <h1>Your Ticket </h1>
+                
+      
             </Grid>
-          </Container>
+        </Container>
+        
+        
+      
+        <Container sx={{marginTop:"10vh", justifyContent:"center", display:"flex"}}>
+            <Box sx={{backgroundColor:"black",width:"55vw",borderRadius:"10px",background: 'linear-gradient(160deg, rgba(117,116,116,1) 0%, rgba(0,0,0,1) 58%, rgba(0,0,0,1) 100%)'}}>
+                <Container sx={{color:"#fff", marginTop:"2vh"}}>
+                
+
+                <Container sx={{justifyContent:"left", marginBottom:"5vh"}}>
+                    <div className="logoDiv">
+                        <img src={logo} style={{width:"10vw", marginTop:"1vh"}} alt="B Airways logo" className="logo" />
+                    </div>
+                    <Typography variant='h2' style={{color:"#fff"}}>
+                        {booking.origin} <ArrowForwardIcon sx={{fontSize:"0.7em"}}/> {booking.destination}
+                    </Typography>
+
+
+
+                    <div style={{display:"flex", marginTop:"2vh"}}>
+                        <div style={{flex: "33%"}}>
+                            <Typography variant='p'>
+                                Aircraft: {booking.call_sign}
+                                <br/>
+                                Seat No: {booking.seat_class}-{booking.seat_number}
+                                <br/>
+                                Class : {
+                                    booking.seat_class === "E" && "Economy"
+                                    || booking.seat_class === "B" && "Business"
+                                    || booking.seat_class === "P" && "Platinum"
+                                }
+                                <br/>
+                                
+                            </Typography>
+                        </div>
+                        <div style={{flex: "33%"}}>
+                            <Typography variant='p'>
+                            Departure : {removeTAndZ(booking.departure_time)}
+                                <br/>
+                                Arrival : {removeTAndZ(booking.arrival_time)}
+                                <br/>
+                            </Typography>
+                        </div>
+                        <div style={{flex: "33%"}}>
+                        <Typography variant='p'>
+                            Name : {booking.name}
+                            <br/>
+                            NIC : {booking.nic}
+                            <br/>
+                            Passport Id : {booking.passport_id} 
+                            </Typography>
+                        </div>
+                    </div>
+
+
+                    
+                    
+                </Container>
+
+                </Container>
+            </Box>
+        </Container>
+                
+        </Container>
+        <Container sx={{marginTop:'5vh', justifyContent:"center", display:"flex"}}>
+            <Grid>
+      
+            {!isButtonHidden && (
+                                <button 
+                                    className="bttn bttnsignin"
+                                    id="myButton"
+                                    variant="contained"
+                                    onClick={combineEvent}
+                                    
+                                >
+                                    Print Ticket
+                                </button>
+                            )}
+                
+      
+            </Grid>
+        </Container>
         </div>
-      </div>
-    </>
-  );
+        </div>
+        
+        </>
+    );
 }
